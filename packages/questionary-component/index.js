bdba8dca6cf6b6b2ef262fcd57d1bdc49fddcfb2
Luis Ricardo Ruiz <luislicardo1307@gmail.com>
2018-06-27 13:15:20 -0500

    Initial commit

---
+++ packages/questionary-component/index.js
@@ -0,0 +1,3 @@
+module.exports = function () {
+    console.log('Adiós!!!')
+}
---
+++ packages/test-component/index.js
@@ -0,0 +1,7 @@
+const region = include('region-component'),
+    questionary = include('questionary-component');
+
+module.exports = function () {
+    region();
+    questionary();
+}
